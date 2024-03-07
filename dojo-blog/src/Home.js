8c2640f249777e398983935ca803ae333d13b44e
Matt Stephens <88216917+Zarlyl@users.noreply.github.com>
2024-03-07 16:54:35 -0500

    Finished lesson 18

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,30 +2,24 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-		{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-		{
-			title: 'Web dev top tips',
-			body: 'lorem ipsum...',
-			author: 'mario',
-			id: 3,
-		},
-	]);
-
-	const handleDelete = (id) => {
-		const newBlogs = blogs.filter((blog) => blog.id !== id);
-		setBlogs(newBlogs);
-	};
+	const [blogs, setBlogs] = useState(null);
 
 	useEffect(() => {
-		console.log('use effect ran');
-		console.log(blogs);
-	});
+		fetch('http://localhost:8080/blogs')
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setBlogs(data);
+			});
+	}, []);
 
 	return (
 		<div className="home">
-			<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+			{/* conditional output - JS evaluates what's on the left and if it's false then the right
+            side is never evaluated. Therefore, it's not output since at load blogs is null which evaluates
+            to false. */}
+			{blogs && <BlogList blogs={blogs} title="All Blogs" />}
 		</div>
 	);
 };
